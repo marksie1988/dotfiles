4ef3b3289076088160fb1a21e9357523e3db539f
marksie1988 <marksie1988@users.noreply.github.com>
2021-05-18 09:05:02 +0100

    update install file NOT TESTED

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # this script should be run via curl:
 #   sh -c "$(wget -qO- https://raw.githubusercontent.com/marksie1988/dotfiles-new/master/install.sh)"
@@ -111,7 +111,7 @@ setup_dotfiles() {
 		-c receive.fsck.zeroPaddedFilemode=ignore \
 		--depth=1 --branch "$BRANCH" "$REMOTE" "$DOTFILES" || {
 		fmt_error "git clone of dotfiles repo failed"
-    	exit 1
+		exit 1
   	}
 
   echo
@@ -126,23 +126,23 @@ setup_fish() {
   # Must use this exact name so uninstall.sh can find it
   OLD_FISHCONF=~/.config/fish-pre-dotfiles
   if [ -d ~/.config/fish ] || [ -h ~/.config/fish ]; then
-    echo "${YELLOW}Found ~/.config/fish.${RESET} ${GREEN}Keeping...${RESET}"
-    return
+	echo "${YELLOW}Found ~/.config/fish.${RESET} ${GREEN}Keeping...${RESET}"
+	return
   
-    if [ -d "$OLD_FISHCONF" ]; then
-      OLD_OLD_FISHCONF="${OLD_FISHCONF}-$(date +%Y-%m-%d_%H-%M-%S)"
-      if [ -e "$OLD_OLD_FISHCONF" ]; then
-        fmt_error "$OLD_OLD_FISHCONF exists. Can't back up ${OLD_FISHCONF}"
-        fmt_error "re-run the installer again in a couple of seconds"
-        exit 1
-      fi
-      mv "$OLD_FISHCONF" "${OLD_OLD_FISHCONF}"
+	if [ -d "$OLD_FISHCONF" ]; then
+	  OLD_OLD_FISHCONF="${OLD_FISHCONF}-$(date +%Y-%m-%d_%H-%M-%S)"
+	  if [ -e "$OLD_OLD_FISHCONF" ]; then
+		fmt_error "$OLD_OLD_FISHCONF exists. Can't back up ${OLD_FISHCONF}"
+		fmt_error "re-run the installer again in a couple of seconds"
+		exit 1
+	  fi
+	  mv "$OLD_FISHCONF" "${OLD_OLD_FISHCONF}"
 
-      echo "${YELLOW}Found old ~/.config/fish-pre-dotfiles." \
-        "${GREEN}Backing up to ${OLD_OLD_FISHCONF}${RESET}"
-    fi
-    echo "${YELLOW}Found ~/.config/fish.${RESET} ${GREEN}Backing up to ${OLD_FISHCONF}${RESET}"
-    mv ~/.config/fish "$OLD_FISHCONF"
+	  echo "${YELLOW}Found old ~/.config/fish-pre-dotfiles." \
+		"${GREEN}Backing up to ${OLD_OLD_FISHCONF}${RESET}"
+	fi
+	echo "${YELLOW}Found ~/.config/fish.${RESET} ${GREEN}Backing up to ${OLD_FISHCONF}${RESET}"
+	mv ~/.config/fish "$OLD_FISHCONF"
   fi
 
   echo "${GREEN}Copying the dotfiles fish config.${RESET}"
@@ -155,77 +155,77 @@ setup_fish() {
 setup_shell() {
   # Skip setup if the user wants or stdin is closed (not running interactively).
   if [ "$CHSH" = no ]; then
-    return
+	return
   fi
 
   # If this user's login shell is already "fish", do not attempt to switch.
   if [ "$(basename -- "$SHELL")" = "fish" ]; then
-    return 
+	return 
   fi
 
   # If this platform doesn't provide a "chsh" command, bail out.
   if ! command_exists chsh; then
-    cat <<EOF
+	cat <<EOF
 I can't change your shell automatically because this system does not have chsh.
 ${BLUE}Please manually change your default shell to fish${RESET}
 EOF
-    return
+	return
   fi
 
   echo "${BLUE}Time to change your default shell to fish:${RESET}"
 
   # Prompt for user choice on changing the default login shell
   printf '%sDo you want to change your default shell to fish? [Y/n]%s ' \
-    "$YELLOW" "$RESET"
+	"$YELLOW" "$RESET"
   read -r opt
   case $opt in
-    y*|Y*|"") echo "Changing the shell..." ;;
-    n*|N*) echo "Shell change skipped."; return ;;
-    *) echo "Invalid choice. Shell change skipped."; return ;;
+	y*|Y*|"") echo "Changing the shell..." ;;
+	n*|N*) echo "Shell change skipped."; return ;;
+	*) echo "Invalid choice. Shell change skipped."; return ;;
   esac
 
   # Check if we're running on Termux
   case "$PREFIX" in
-    *com.termux*) termux=true; fish=fish ;;
-    *) termux=false ;;
+	*com.termux*) termux=true; fish=fish ;;
+	*) termux=false ;;
   esac
 
   if [ "$termux" != true ]; then
-    # Test for the right location of the "shells" file
-    if [ -f /etc/shells ]; then
-      shells_file=/etc/shells
-    elif [ -f /usr/share/defaults/etc/shells ]; then # Solus OS
-      shells_file=/usr/share/defaults/etc/shells
-    else
-      fmt_error "could not find /etc/shells file. Change your default shell manually."
-      return
-    fi
+	# Test for the right location of the "shells" file
+	if [ -f /etc/shells ]; then
+	  shells_file=/etc/shells
+	elif [ -f /usr/share/defaults/etc/shells ]; then # Solus OS
+	  shells_file=/usr/share/defaults/etc/shells
+	else
+	  fmt_error "could not find /etc/shells file. Change your default shell manually."
+	  return
+	fi
 
-    # Get the path to the right fish binary
-    # 1. Use the most preceding one based on $PATH, then check that it's in the shells file
-    # 2. If that fails, get a fish path from the shells file, then check it actually exists
-    if ! fish=$(command -v fish) || ! grep -qx "$fish" "$shells_file"; then
-      if ! fish=$(grep '^/.*/fish$' "$shells_file" | tail -1) || [ ! -f "$fish" ]; then
-        fmt_error "no fish binary found or not present in '$shells_file'"
-        fmt_error "change your default shell manually."
-        return
-      fi
-    fi
+	# Get the path to the right fish binary
+	# 1. Use the most preceding one based on $PATH, then check that it's in the shells file
+	# 2. If that fails, get a fish path from the shells file, then check it actually exists
+	if ! fish=$(command -v fish) || ! grep -qx "$fish" "$shells_file"; then
+	  if ! fish=$(grep '^/.*/fish$' "$shells_file" | tail -1) || [ ! -f "$fish" ]; then
+		fmt_error "no fish binary found or not present in '$shells_file'"
+		fmt_error "change your default shell manually."
+		return
+	  fi
+	fi
   fi
 
   # We're going to change the default shell, so back up the current one
   if [ -n "$SHELL" ]; then
-    echo "$SHELL" > ~/.shell.pre-dotfiles
+	echo "$SHELL" > ~/.shell.pre-dotfiles
   else
-    grep "^$USERNAME:" /etc/passwd | awk -F: '{print $7}' > ~/.shell.pre-dotfiles
+	grep "^$USERNAME:" /etc/passwd | awk -F: '{print $7}' > ~/.shell.pre-dotfiles
   fi
 
   # Actually change the default shell to fish
   if ! chsh -s "$fish"; then
-    fmt_error "chsh command unsuccessful. Change your default shell manually."
+	fmt_error "chsh command unsuccessful. Change your default shell manually."
   else
-    export SHELL="$fish"
-    echo "${GREEN}Shell successfully changed to '$fish'.${RESET}"
+	export SHELL="$fish"
+	echo "${GREEN}Shell successfully changed to '$fish'.${RESET}"
   fi
 
   echo
@@ -235,30 +235,30 @@ EOF
 main() {
   # Run as unattended if stdin is not a tty
   if [ ! -t 0 ]; then
-    RUNFISH=no
-    CHSH=no
+	RUNFISH=no
+	CHSH=no
   fi
 
   # Parse arguments
   while [ $# -gt 0 ]; do
-    case $1 in
-      --unattended) RUNFISH=no; CHSH=no ;;
-      --skip-chsh) CHSH=no ;;
-    esac
-    shift
+	case $1 in
+	  --unattended) RUNFISH=no; CHSH=no ;;
+	  --skip-chsh) CHSH=no ;;
+	esac
+	shift
   done
 
   setup_color
 
   if ! command_exists fish; then
-    echo "${YELLOW}Fish is not installed.${RESET} Please install fish first."
-    exit 1
+	echo "${YELLOW}Fish is not installed.${RESET} Please install fish first."
+	exit 1
   fi
 
   if [ -d "$DOTFILES" ]; then
-    echo "${YELLOW}The \$DOTFILES folder already exists ($DOTFILES).${RESET}"
-    if [ "$custom_dotfiles" = yes ]; then
-      cat <<EOF
+	echo "${YELLOW}The \$DOTFILES folder already exists ($DOTFILES).${RESET}"
+	if [ "$custom_dotfiles" = yes ]; then
+	  cat <<EOF
 
 You ran the installer with the \$DOTFILES setting or the \$DOTFILES variable is
 exported. You have 3 options:
@@ -271,10 +271,10 @@ exported. You have 3 options:
    you can just remove it with $(fmt_code "rm -r $DOTFILES")
 
 EOF
-    else
-      echo "You'll need to remove it if you want to reinstall."
-    fi
-    exit 1
+	else
+	  echo "You'll need to remove it if you want to reinstall."
+	fi
+	exit 1
   fi
 
   setup_dotfiles
@@ -288,8 +288,8 @@ EOF
   printf %s "$RESET"
 
   if [ $RUNDOTFILES = no ]; then
-    echo "${YELLOW}Run fish to try it out.${RESET}"
-    exit
+	echo "${YELLOW}Run fish to try it out.${RESET}"
+	exit
   fi
 
   exec fish -l
