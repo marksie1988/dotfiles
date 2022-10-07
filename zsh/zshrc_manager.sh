99f87b85b8869bf7216d74c00327960e347ff7bf
marksie1988 <marksie1988@users.noreply.github.com>
2022-10-07 12:28:42 +0100

    new zshrc

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,15 +1,15 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
 echo "Checking for new dotfiles release."
-current = yadm describe --tags
-get_hash = yadm --work-tree=../ rev-list --tags='v*' --max-count=1 2> /dev/null
-latest = yadm --work-tree=../ describe --tags $get_hash
+current=yadm describe --tags
+get_hash=yadm --work-tree=../ rev-list --tags='v*' --max-count=1 2> /dev/null
+latest=yadm --work-tree=../ describe --tags $get_hash
 
 echo "Current: "$current" Latest:" $latest
 
 ({yadm fetch -q} &> /dev/null)
 
-if [ yadm rev-list HEAD...origin/master | wc -l) = 0 ]
+if [ $(yadm rev-list HEAD...origin/master | wc -l) = 0 ]
 then
 	echo "Already up-to-date."
 else
