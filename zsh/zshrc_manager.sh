915bfe0af8a81e79a2fdc50cc20f235658fba23a
marksie1988 <marksie1988@users.noreply.github.com>
2022-10-07 12:45:01 +0100

    update manager

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -1,21 +1,27 @@
 time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
-echo "Checking for new dotfiles release."
-CURRENT=yadm describe --tags
-GETHASH=yadm --work-tree=../ rev-list --tags='v*' --max-count=1 2> /dev/null
-LATEST=yadm --work-tree=../ describe --tags $GETHASH
+echo " Checking for new dotfiles release..."
 
-echo "Current: "$CURRENT" Latest:" $LATEST
+current() {
+    yadm describe --tags
+}
+hash(){
+	yadm --work-tree=../ rev-list --tags='v*' --max-count=1 2> /dev/null
+}
+latest() {
+    yadm --work-tree=../ describe --tags $(hash)
+}
+echo "Current: $(current) Latest: $(latest)"
 
 ({yadm fetch -q} &> /dev/null)
 
 if [ $(yadm rev-list HEAD...origin/master | wc -l) = 0 ]
 then
-	echo "Already up-to-date."
+	echo " Already up-to-date."
 else
-	echo "Updates Detected:"
+	echo " Updates Detected:"
 	(yadm log ..@{u} --pretty=format:%Cred%aN:%Creset\ %s\ %Cgreen%cd)
-	echo "Pulling Updates..."
+	echo " Pulling Updates..."
 	(yadm pull -q && yadm submodule update --init --recursive)
 fi
 
