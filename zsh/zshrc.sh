06825731e520929841b067befc9005854b260ce0
marksie1988 <marksie1988@users.noreply.github.com>
2022-10-09 20:32:41 +0100

    added history

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -15,6 +15,16 @@ fix_wsl2_interop() {
     done
 }
 
+# History
+# --
+#
+mkdir -p ~/.cache/zsh/
+HISTFILE=~/.cache/zsh/history
+HISTSIZE=1000
+SAVEHIST=1000
+setopt inc_append_history # To save every command before it is executed
+setopt share_history # setopt inc_append_history
+
 # Default Editor
 # ---
 #
