1e43758649050bd77db18f05b1703411aa691f58
marksie1988 <marksie1988@users.noreply.github.com>
2023-04-20 11:13:12 +0100

    feat: upgrades exa and starship to latest versions

--- zsh/zshrc_manager.sh
+++ zsh/zshrc_manager.sh
@@ -2,30 +2,61 @@ time_out () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 printf '%.s─' $(seq 1 $(tput cols))
 
 echo "Checking starship is installed"
+
+starship_install() {
+	cd /tmp
+	curl -s https://api.github.com/repos/starship/starship/releases/latest \
+	| grep browser_download_url \
+	| grep x86_64-unknown-linux-gnu \
+	| cut -d '"' -f 4 \
+	| wget -qi -
+	tar xvf starship-*.tar.gz
+	sudo mv starship /usr/local/bin/
+	starship -V
+}
 if ! [ -x "$(command -v starship)" ]; then
-  echo "Installing Starship..."
-  cd /tmp
-  curl -s https://api.github.com/repos/starship/starship/releases/latest \
-  | grep browser_download_url \
-  | grep x86_64-unknown-linux-gnu \
-  | cut -d '"' -f 4 \
-  | wget -qi -
-  tar xvf starship-*.tar.gz
-  sudo mv starship /usr/local/bin/
-  starship -V
+  	echo "Installing Starship..."
+	$(starship_install)
+else
+	starship_latest() {
+		curl -s https://api.github.com/repos/starship/starship/releases/latest | grep tag_name | cut -d '"' -f 4
+	}
+	starship_current() {
+		starship -V | grep starship | cut -d ' ' -f 2
+	}
+	if [ $(starship_latest) != $(starship_current) ]; then
+		echo "Upgrading Starship..."
+		$(starship_install)
+	fi
 fi
 
 echo "Checking exa is installed"
+exa_install() {
+	cd /tmp
+	curl -s https://api.github.com/repos/ogham/exa/releases/latest \
+	| grep browser_download_url \
+	| grep linux-x86_64-v \
+	| cut -d '"' -f 4 \
+	| wget -qi -
+	unzip exa-*.zip -f /tmp/exa
+	sudo mv exa/bin/exa /usr/local/bin/
+	mkdir -p ~/zsh/plugins/exa
+	mv /tmp/exa/completions/exa.zsh ~/zsh/plugins/exa/
+}
 if  ! [ -x "$(command -v exa)" ]; then
-  echo "Installing exa..."
-  cd /tmp
-  curl -s https://api.github.com/repos/ogham/exa/releases/latest \
-  | grep browser_download_url \
-  | grep linux-x86_64-v \
-  | cut -d '"' -f 4 \
-  | wget -qi -
-  unzip exa-*.zip -f /tmp/exa
-  sudo mv exa/bin/exa /usr/local/bin/
+  	echo "Installing exa..."
+	$(exa_install)
+else
+	exa_latest() {
+		curl -s https://api.github.com/repos/ogham/exa/releases/latest | grep tag_name | cut -d '"' -f 4
+	}
+	exa_current() {
+		exa --version | grep v | cut -d " " -f 1
+	}
+	if [ $(exa_latest) != $(exa_current) ]; then
+		echo "Upgrading exa..."
+		$(exa_install)
+	fi
 fi
 
 echo "Checking yadm is installed"
@@ -67,11 +98,6 @@ fi
 # Check submodules are all installed and updated
 cd ~
 yadm submodule update --init --recursive -q
-
-# Check exa completions are installed:
-mkdir -p ~/zsh/plugins/exa
-mv /tmp/exa/completions/exa.zsh ~/zsh/plugins/exa/
-
 
 printf '%.s─' $(seq 1 $(tput cols))
 source ~/zsh/zshrc.sh
