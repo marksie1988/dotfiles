87f75358403c4f21330035c7c92e1a32f4af63f9
Steven Marks <marksie1988@users.noreply.github.com>
2023-05-09 21:42:56 +0100

    feat: add more aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,23 +1,33 @@
 alias nrb='npm run build'
 alias sha256='shasum -a 256'
 
+# Easier navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ~="cd ~" # `cd` is probably faster to type though
+alias -- -="cd -"
+
 # Git Aliases
+alias g="git"
 alias gs='git status -s'
 alias gf='git fetch --prune'
-alias gpm='git pull origin master'
-alias gcb='git checkout -b'
+alias gpl='git pull'
+alias gplr='git pull --rebase'
+alias gps='git push'
+alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'alias gcb='git checkout -b'
+alias grs='git reset'
+alias grsh='git reset --hard'
 alias gcm='git add -A && git commit -m'
 alias undo='git reset HEAD~1 --mixed'
 alias gr='git remote -v'
 alias ssha='eval $(ssh-agent) && ssh-add'
 
 # Automtion
-alias k="kubectl"
-alias h="helm"
 alias tf="terraform"
 alias a="ansible"
 alias ap="ansible-playbook"
-alias dt="datree"
 
 
 # ALIAS COMMANDS
@@ -26,3 +36,15 @@ alias ll="exa --icons --group-directories-first -l"
 alias tree="exa --tree --icons"
 alias g="goto"
 alias grep='grep --color'
+
+# IP Information
+alias pubip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Print each PATH entry on a separate line
+alias path='echo -e ${PATH//:/\\n}'
+
+# Show human friendly numbers and colors
+alias df='df -h'
+alias du='du -h -d 2'
