f268b28523d76c9a06eda873c62135855decf910
marksie1988 <marksie1988@users.noreply.github.com>
2024-05-09 12:10:07 +0100

    upgrade starship styling

--- .zsh/distribution.sh
+++ .zsh/distribution.sh
@@ -1,10 +1,18 @@
 # find out which distribution we are running on
-LFILE="/etc/*-release"
 MFILE="/System/Library/CoreServices/SystemVersion.plist"
-if [[ -f $LFILE ]]; then
+if ls /etc/*-release 1> /dev/null 2>&1; then
   _distro=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
 elif [[ -f $MFILE ]]; then
   _distro="macos"
+
+  # on mac os use the systemprofiler to determine the current model
+  _device=$(system_profiler SPHardwareDataType | awk '/Model Name/ {print $3,$4,$5,$6,$7}')
+
+  case $_device in
+    *MacBook*)     DEVICE="";;
+    *mini*)        DEVICE="󰇄";;
+    *)             DEVICE="";;
+  esac
 fi
 
 # set an icon based on the distro
@@ -36,3 +44,4 @@ case $_distro in
 esac
 
 export STARSHIP_DISTRO="$ICON"
+export STARSHIP_DEVICE="$DEVICE"
