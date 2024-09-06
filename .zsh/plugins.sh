310696a3eb4b79eec1a6feabf1368a80b4afbe00
Steven Marks <marksie1988@users.noreply.github.com>
2024-09-06 21:25:10 +0100

    fix: make git silent

--- .zsh/plugins.sh
+++ .zsh/plugins.sh
@@ -2,16 +2,16 @@ autoload -U compinit
 
 if [ ! -d ~/.zsh/plugins/zsh-autosuggestions ]; then
     echo "Installing zsh-autosuggestions"
-    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions >/dev/null 2>&1
 else 
-    git -C ~/.zsh/plugins/zsh-autosuggestions pull
+    git -C ~/.zsh/plugins/zsh-autosuggestions pull >/dev/null 2>&1
 fi
 
 if [ ! -d '~/.zsh/plugins/zsh-syntax-highlighting' ]; then
     echo "Installing zsh-syntax-highlighting"
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/plugins/zsh-syntax-highlighting >/dev/null 2>&1
 else 
-    git -C ~/.zsh/plugins/zsh-syntax-highlighting pull
+    git -C ~/.zsh/plugins/zsh-syntax-highlighting pull >/dev/null 2>&1
 fi
 
 plugins=(
