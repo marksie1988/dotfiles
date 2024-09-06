c78ce76857da734c43f0d3eb7754d2db430e36db
Steven Marks <marksie1988@users.noreply.github.com>
2024-09-06 21:07:57 +0100

    fix: missing not on plugins

--- .zsh/plugins.sh
+++ .zsh/plugins.sh
@@ -1,11 +1,11 @@
 autoload -U compinit
 
-if [ -f '~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' ]; then
+if [ ! -f '~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' ]; then
     echo "Installing zsh-autosuggestions"
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
 fi
 
-if [ -f '~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' ]; then
+if [ ! -f '~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' ]; then
     echo "Installing zsh-syntax-highlighting"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/plugins/zsh-syntax-highliting
 fi
